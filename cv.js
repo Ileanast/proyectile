5b037f667bc4b610f9b96bff5e74b9dc627cc7f0
Ileana Stanganelli <ileanastanganelli87@gmail.com>
2023-03-20 11:44:01 -0300

    publicación cv

---
+++ cv.js
@@ -0,0 +1,14 @@
+document.getElementById ("direccion").onclick = function () {
+    console.log ("capturamos el evento click")
+    document.getElementById ("respuesta dirccion").innerHTML = ("Asunción 5204 CABA")
+}
+    
+document.getElementById ("celular").onclick = function () {
+    console.log ("capturamos el evento")
+    document.getElementById ("respuesta celular").innerHTML = ("11-3755-5129")
+}
+
+document.getElementById ("mail").onclick = function () {
+    console.log ("capturamos el evento")
+    document.getElementById ("respuesta mail").innerHTML = ("ileanastanganelli87@gmail.com")
+}
